fe480e904b89a832efe4a4785f2c33f0886f62e5
Alejo <alejo12arias@gmail.com>
2022-03-27 14:40:36 -0300

    Mas proyectos

---
+++ 6practico/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Celular celular = new Celular();
+		
+		celular.EncenderCelular();
+		celular.ReiniciarCelular();
+		celular.ApagarCelular();
+		
+		celular.PedirDatos();
+		celular.Info();
+	}
+
+}
---
+++ 8practico/src/Practicando.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class Practicando {
+
+	Scanner sc = new Scanner(System.in);
+	
+	/*int edad;
+	String nombre;
+	String apellido;*/
+	//int b;
+	
+	public static void main(String[] args) {
+		Practicando prog = new Practicando();
+		
+		/*prog.PedirDatos();
+		prog.calculo();
+		prog.Mostrar();*/
+
+		//prog.ascensor(0);
+	}
+	
+	// PRACTICA CALCULAR EDAD Y MOSTRAR DATOS EN PANTALLA 
+	
+	/*public void PedirDatos() {
+		
+		System.out.println("Ingrese su edad: ");
+		edad = sc.nextInt();
+		System.out.println("Ingrese su nombre: ");
+		nombre = sc.next();
+		System.out.println("Ingrese su apellido: ");
+		apellido = sc.next();
+		
+	}
+	
+	
+	
+	public void Mostrar() {
+		System.out.println("Su edad es: " + edad);
+		System.out.println("Su nombre es: " + nombre);
+		System.out.println("Su apellido es: " + apellido);
+	}
+	
+	public void calculo() {
+		 if (edad < 18) {
+			System.out.println("Sos menor de edad");
+		} else {
+			System.out.println("Sos mayor de 18");
+		}
+	}*/
+	
+	// PRACTICA CALCULAR EDAD Y MOSTRAR DATOS EN PANTALLA 
+	
+	//---------------------------------------------------------------------
+	
+	//ASCENSOR PROGRAMA/ FOR
+	
+	/*public void ascensor(int b) {
+		System.out.println("Ingrese numero de piso: ");
+		b = sc.nextInt();
+		
+		for (int i = 1; i <= b; i++) {
+			System.out.println("Sube al piso: " + i);
+		}
+	}*/
+	
+	//ASCENSOR PROGRAMA/ FOR//
+	
+	//----------------------------------------------------------------
+	
+	
+
+}
